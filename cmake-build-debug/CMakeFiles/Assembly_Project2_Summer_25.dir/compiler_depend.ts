99e5abda17bdf6d32f86093132d4d5ee7b3cc92e
Mahinour <mahinourabdelgawad@aucegypt.edu>
2025-07-21 20:29:38 +0300

    added gitignore and removed build files

--- cmake-build-debug/CMakeFiles/Assembly_Project2_Summer_25.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for Assembly_Project2_Summer_25.
--- cmake-build-debug/CMakeFiles/Assembly_Project2_Summer_25.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Assembly_Project2_Summer_25.
-# This may be replaced when dependencies are built.
